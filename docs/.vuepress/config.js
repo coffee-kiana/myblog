6b7aecd58f146f9fe3fd2024606209ea9c1f97c0
zhiyannan <289926628@qq.com>
2022-01-26 07:28:14 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
     title: 'Kiana\'s BLog',
     description: 'personal blog',
     theme: 'reco',
+    base: '/myblog/',
     locales: {
         '/': {
         lang: 'zh-CN'
@@ -11,22 +12,6 @@ module.exports = {
         nav: [
             { text: 'home', link: '/' },
             {text:'note',link:'/note/test'},
-      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
-    ],
-        // 博客配置
-        blogConfig: {
-            category: {
-            location: 2,     // 在导航栏菜单中所占的位置，默认2
-            text: 'Category' // 默认文案 “分类”
-            },
-            tag: {
-            location: 3,     // 在导航栏菜单中所占的位置，默认3
-            text: 'Tag'      // 默认文案 “标签”
-            },
-            socialLinks: [     // 信息栏展示社交信息
-            { icon: 'reco-github', link: 'https://github.com/recoluan' },
-            { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
-            ]
-        }
+    ],  
     }
 }
